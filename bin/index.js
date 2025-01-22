10e3d2b4f0df5c2b3ccc3f196f651890ff392a22
Adithya Menon R <adukottan@gmail.com>
2025-01-22 18:36:36 +0530

    Moved clear command logic to bin/util/clear.js

--- bin/index.js
+++ bin/index.js
@@ -12,7 +12,7 @@ import { metadata, commands, templates } from "./configs.js";
 import validate from "validate-npm-package-name";
 import { getServicesData, generateDockerComposeFile } from "./util/docker.js";
 import { initMenu } from "./util/menu.js";
-import { confirm } from "@inquirer/prompts";
+import { clearCWD } from "./util/clear.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -88,57 +88,8 @@ program
 program
     .command(commands.clear.command)
     .description(commands.clear.description)
-    .action(async () => {
-        const targetDir = process.cwd();
-        console.log(
-            chalk.yellow.bold("\nWARNING: This operation is irreversible."),
-        );
-        console.log(
-            chalk.white("You are about to delete all files in the directory:"),
-            chalk.red.bold(targetDir),
-            "\n",
-        );
-
-        const clearConfirmation = await confirm({
-            message: "Are you sure you want to proceed? (Default: No)",
-            default: false,
-        });
-
-        if (clearConfirmation) {
-            const clearingDirectory = createSpinner(
-                "Scanning project directory...",
-            ).start();
-            try {
-                const files = fs.readdirSync(targetDir);
-
-                clearingDirectory.update({
-                    text: "Deleting all files...",
-                });
-
-                for (const file of files) {
-                    const filePath = path.join(targetDir, file);
-                    fs.removeSync(filePath);
-                }
-
-                clearingDirectory.success({
-                    text: chalk.green.bold(
-                        "Successfully cleared project directory.",
-                    ),
-                });
-            } catch (error) {
-                clearingDirectory.error({
-                    text: chalk.red.bold("Error clearing project directory."),
-                });
-                console.error(error);
-            }
-        } else {
-            const cancelSpinner = createSpinner(
-                "Cancelling operation...",
-            ).start();
-            cancelSpinner.success({
-                text: chalk.white("Operation cancelled successfully."),
-            });
-        }
+    .action(() => {
+        clearCWD();
     });
 
 async function initCommand(options) {
